b30d024c4254bed5768a18e5e733821e77809378
Sheila Nyambura <sheilanyambura394@gmail.com>
2023-12-19 17:51:45 +0300

    done

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -2,6 +2,10 @@
 
 import ipdb
 
-def plus_two(num):
-    num + 2
-    return num
+def plus_two(num=3):
+    total = num + 2
+    # ipdb.set_trace()
+    print(total)
+    return total
+
+plus_two()
